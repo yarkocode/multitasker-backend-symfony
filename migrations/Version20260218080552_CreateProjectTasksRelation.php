aa55f52e3ffafba3a5dc158eae2420cdeba45b0a
Yaroslav* <yarkobout@yandex.ru>
2026-02-18 16:14:31 +0700

    refactor: clean

--- migrations/Version20260218080552_CreateProjectTasksRelation.php
+++ migrations/Version20260218080552_CreateProjectTasksRelation.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260218080552CreateProjectTasksRelation extends AbstractMigration
+final class Version20260218080552_CreateProjectTasksRelation extends AbstractMigration
 {
     public function getDescription(): string
     {
