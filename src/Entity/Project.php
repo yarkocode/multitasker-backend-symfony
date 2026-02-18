1f3c7593604d40c185d42db5b48d41633c2429c4
Yaroslav* <yarkobout@yandex.ru>
2026-02-18 13:58:36 +0700

    dev(entity): user project workplace to store tasks

---
+++ src/Entity/Project.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\Abstract\Auditable;
+use App\Entity\Abstract\AuditableInterface;
+use App\Repository\ProjectRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProjectRepository::class)]
+#[ORM\Table(name: 'projects')]
+class Project implements AuditableInterface
+{
+    use Auditable;
+
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private string $name;
+
+    #[ORM\Column(type: 'text', nullable: true)]
+    private ?string $description = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+        return $this;
+    }
+}
